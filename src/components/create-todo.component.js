62a8a2c5a434097f6abf04bde5aece65a1542726
Emp-Developer <emperor95313@gmail.com>
2021-03-09 05:38:24 -0400

    creating basic components

---
+++ src/components/create-todo.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CreateTodo extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to Create Todo!
+            </div>
+        )
+    }
+}
+
+export default CreateTodo
---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class EditTodo extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to Edit Todo!
+            </div>
+        )
+    }
+}
+
+export default EditTodo
